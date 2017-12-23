4aa77fdfcf3a2f9004846793e0ebd09b52d190d3
Diego Alberto Farías Pineda <diego@linux.suse>
2017-12-23 17:35:15 -0600

    Imprime versión de la aplicación en método Main

--- src/main/java/mx/com/wedev/App.java
+++ src/main/java/mx/com/wedev/App.java
@@ -1,5 +1,9 @@
 package mx.com.wedev;
 
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	try {
+	    	Properties properties = new Properties();
+	    	InputStream propertiesStream 
+	    		= App.class.getClassLoader().getResourceAsStream("app.properties");
+	    	properties.load(propertiesStream);
+	        System.out.println( "Hello World!" );
+	        System.out.println( "Application version: " + properties.get("application.version") );
+    	} catch (IOException ex) {
+    		System.err.println( "Error loading properties file" );
+    		ex.printStackTrace();
+    		System.exit(1);
+    	}
     }
 }
